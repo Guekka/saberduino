f8f5a5fbb09799215a386b50e77e3e35087cfc67
Edgar B <epersonnal@keemail.me>
2021-12-13 16:12:28 +0100

    Add time functions

--- src/Saberduino/saberduino/base_defines.hpp
+++ src/Saberduino/saberduino/base_defines.hpp
@@ -2,8 +2,10 @@
 #define SABERDUINO_BASE_DEFINES
 
 #ifdef SABERDUINO_DESKTOP_COMPUTER  // Assume Arduino available
+#include <chrono>
 #include <cstdint>
 #include <iostream>
+#include <thread>
 #else
 #include "Arduino.h"
 #endif
@@ -12,5 +14,29 @@ struct Position {
     uint8_t x;
     uint8_t y;
 };
+
+inline uint16_t to_millis_scaled(uint64_t millis) {
+    return static_cast<uint16_t>(millis / 32);
+}
+
+inline uint64_t milli_scaled() {
+#ifdef SABERDUINO_DESKTOP_COMPUTER
+    using namespace std::chrono;
+    static const auto start = steady_clock::now();
+    const auto now = steady_clock::now();
+    const auto millis = duration_cast<milliseconds>(now - start);
+    return to_millis_scaled(millis.count());
+#else
+    return millis() / 20;
+#endif
+}
+
+inline void sleep_milli(uint16_t millis) {
+#ifdef SABERDUINO_DESKTOP_COMPUTER
+    std::this_thread::sleep_for(std::chrono::milliseconds(millis));
+#else
+    delay(millis);
+#endif
+}
 
 #endif
