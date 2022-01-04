5d0a99b77c932a62eabb29d2e040560d03e63745
G'k <39066502+Guekka@users.noreply.github.com>
2022-01-04 16:39:37 +0100

    Add missing file

---
+++ src/Saberduino/main/main.ino
@@ -0,0 +1,35 @@
+#ifndef SABERDUINO_DESKTOP_COMPUTER 
+
+#include "display.hpp"
+#include "level.hpp"
+#include "saber.hpp"
+
+Saber saber;
+Level level;
+Display display;
+
+const Block default_blocks[10] = {
+    Block{{4, 4}, 50},  Block{{5, 5}, 100}, Block{{6, 6}, 150},
+    Block{{7, 7}, 200}, Block{{8, 8}, 250},
+};
+
+void setup() {
+    saber.init();
+    level.init();
+    level.load(default_blocks);
+    level.start();
+}
+
+void loop() {
+    display.begin_frame();
+    level.update(display, saber.position());
+    display.end_frame();
+}
+
+int main() {
+    setup();
+    while (true)
+        loop();
+}
+
+#endif
